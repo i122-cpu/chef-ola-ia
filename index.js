6db4adb641561a10acbf808bce08f44cf80c1231
Rich-cpu <rodrichchoco@gmail.com>
2026-05-24 15:50:45 +0000

    Add route to serve index.html for /app endpoint

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "landing.html"));
 });
 
+
 app.get("/app", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 app.get("/landing.html", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "landing.html"));
 });
