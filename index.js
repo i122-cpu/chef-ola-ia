5ffb37abb54ce857384696e93de49bb2a1334058
Rich-cpu <rodrichchoco@gmail.com>
2026-05-24 21:45:17 +0100

    Change route to serve app.html instead of index.html

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/app", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "app.html"));
 });
 
 // ✅ Static middleware APRES les routes
